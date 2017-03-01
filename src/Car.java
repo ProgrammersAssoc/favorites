7834505ea8cc88ac693f2d5d727c33befcfe04df
Zachary Peters <zp353196@wne.edu>
2017-03-01 14:52:24 -0500

    Created Car class.

---
+++ src/Car.java
@@ -0,0 +1,15 @@
+
+public class Car {
+	
+	private String type;
+	private String brand;
+	private  String doors;
+	
+	public Car(String type, String brand, String doors){
+		
+		this.type = type;
+		this.brand = brand;
+		this.doors = doors;
+	}
+	
+}
