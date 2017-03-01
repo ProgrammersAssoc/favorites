85fe40d4967c248a6b670e199d0fbb5865fb8ff4
Nick Dion <nd363083@wne.edu>
2017-03-01 15:06:20 -0500

    Created Truck Class from Car

---
+++ src/Truck.java
@@ -0,0 +1,20 @@
+public class Truck extends Car {
+	protected int cargo;
+  protected String[] things;
+
+
+	public Truck(String type, String brand, String doors, int c){
+		super(type,brand,doors);
+    cargo = c;
+    things = new String[cargo];
+
+	}
+
+  public int getCargo(){
+    return cargo;
+  }
+  public String getItem(int i);
+  return things[i];
+  
+
+}
